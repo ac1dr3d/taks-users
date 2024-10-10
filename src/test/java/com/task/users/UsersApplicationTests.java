e87498903e9432ea3ede9678f696e587512c9721
Akaki Tkemaladze <a.tkemal311@gmail.com>
2024-10-10 22:29:02 +0400

    +add: project files

---
+++ src/test/java/com/task/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
